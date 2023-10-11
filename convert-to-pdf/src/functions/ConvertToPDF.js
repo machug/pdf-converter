974a91615fea8b84c3bd84bba68c6903c6a63848
Hugh McIntyre <hugh.mcintyre@logicalis.com>
2023-10-12 09:24:57 +1100

    docker and function for puppeteer  local chrome

--- convert-to-pdf/src/functions/ConvertToPDF.js
+++ convert-to-pdf/src/functions/ConvertToPDF.js
@@ -4,59 +4,71 @@ const puppeteer = require("puppeteer");
 app.http("ConvertToPDF", {
   methods: ["POST"],
   handler: async (req, context) => {
-    context.log("Starting conversion process...");
 
-    // Log the received request headers and body for debugging
-    context.log("Headers:", JSON.stringify(req.headers));
-    context.log("Body:", JSON.stringify(req.body));
+    context.log("Request method:", req.method);
 
-    if (req.body && req.body.html) {
-      const htmlContent = req.body.html;
-      context.log("HTML content received.");
+    // extract JSON payload from body
+    const body = await req.json();
 
-      try {
-        // Create a browser instance and open a new page
-        context.log("Launching browser...");
-        const browser = await puppeteer.launch({
-          args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage",
-          ],
-        });
+    context.log("Body:", body);
 
-        const page = await browser.newPage();
-        context.log("Browser launched.");
-
-        // Set HTML content to the page
-        await page.setContent(htmlContent);
-        context.log("HTML content loaded.");
-
-        // Print the page as a PDF
-        const pdfBuffer = await page.pdf({ format: "A4" });
-
-        await browser.close();
-        context.log("Browser closed.");
-
-        return {
-          status: 200,
-          body: pdfBuffer,
-          headers: {
-            "Content-Type": "application/pdf",
-          },
-        };
-      } catch (error) {
-        context.log("Error Message:", error.message);
-        context.log("Error Stack:", error.stack);
-        return {
-          status: 500,
-          body: "Error converting HTML to PDF: " + JSON.stringify(error),
-        };
-      }
-    } else {
+    // Check if the request body contains HTML content
+    if (!body || !body.html) {
       return {
         status: 400,
-        body: "Please provide HTML in the request body.",
+        body: "HTML content is missing in the request body.",
+      };
+    }
+
+    // Log the received request headers and body for debugging
+    const htmlContent = body.html;
+    context.log("HTML content received.");
+    context.log("Starting conversion process...");
+
+    try {
+      // Create a browser instance and open a new page
+      context.log("Launching browser...");
+      const browser = await puppeteer.launch({
+        headless: 'new',
+        executablePath: '/usr/local/bin/chromium/chrome',
+        args: ['--no-sandbox']
+      });
+
+      // Check if the browser instance was created successfully
+      if (!browser) {
+        context.log("Browser instance not created.");
+        return {
+          status: 500,
+          body: "Error creating browser instance.",
+        };
+      }
+
+      const page = await browser.newPage();
+      context.log("Browser launched.");
+
+      // Set HTML content to the page
+      await page.setContent(htmlContent);
+      context.log("HTML content loaded.");
+
+      // Print the page as a PDF
+      const pdfBuffer = await page.pdf({ format: "A4" });
+
+      await browser.close();
+      context.log("Browser closed.");
+
+      return {
+        status: 200,
+        body: pdfBuffer,
+        headers: {
+          "Content-Type": "application/pdf",
+        },
+      };
+    } catch (error) {
+      context.log("Error Message:", error.message);
+      context.log("Error Stack:", error.stack);
+      return {
+        status: 500,
+        body: "Error converting HTML to PDF: " + JSON.stringify(error),
       };
     }
   },
